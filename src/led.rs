e1c9e2f9e49bc8115541d4224c53e4313b583206
julien <julien@eudine.fr>
2023-05-07 01:23:20 +0200

    Drop State0 LED implemented

--- src/led.rs
+++ src/led.rs
@@ -17,7 +17,18 @@ pub fn init(phase: &Arc<Mutex<fft::Phase>>) -> Result<(), Box<dyn std::error::Er
 		};
 		let cmd = match phase.state {
 			fft::State::Break(_) => MpsseCmdBuilder::new().set_gpio_lower(0x0, 0xFF),
-			fft::State::Drop(_) => MpsseCmdBuilder::new().set_gpio_lower(0xFF, 0xFF),
+			fft::State::Drop(d) => match d {
+				fft::Drop::State0 => {
+					let mut val = 0;
+					if phase.gains[0] > 0.5 {
+						val |= 0x03;
+					}
+					if phase.gains[3] > 0.5 {
+						val |= 0x0C;
+					}
+					MpsseCmdBuilder::new().set_gpio_lower(val, 0xFF)
+				}
+			},
 		};
 		ft232h.write_all(cmd.as_slice()).unwrap();
 	});
