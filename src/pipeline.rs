2a224f9a6ee53c19f121fce3f2c73684e126ecd9
julien <julien@eudine.fr>
2023-12-09 19:26:02 +0100

    rustfmt added

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -7,8 +7,19 @@ struct Pipeline {
 	models: Vec<Model>,
 }
 
+struct Layouts {
+	layout_3d: wgpu::PipelineLayout,
+	layout_2d: wgpu::PipelineLayout,
+}
+
 impl Pipeline {
+	fn init_layouts(
+		camera_bind_group_layout: &wgpu::BindGroupLayout,
+		audio_bind_group_layout: &wgpu::BindGroupLayout,
+	) -> Layouts {
+	}
 	fn new_3d(
+		pipeline_layouts: PipelineLayouts,
 		device: wgpu::Device,
 		models: Vec<Model>,
 		shader_path: &std::path::Path,
@@ -22,6 +33,17 @@ impl Pipeline {
 			//TODO: unwrap
 			source: wgpu::ShaderSource::Wgsl(fs::read_to_string(shader_path).unwrap().into()),
 		});
+
+		let render_pipeline_layout =
+			device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+				label: Some("Render Pipeline Layout"),
+				bind_group_layouts: &[
+					&camera_bind_group_layout,
+					&audio_bind_group_layout,
+					&time_bind_group_layout,
+				],
+				push_constant_ranges: &[],
+			});
 	}
 	fn new_2d() {}
 }
